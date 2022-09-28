13c58ae59d7caa399b056085664941ff2732ec8d
John Wyman <Jwyman4821@gmail.com>
2022-09-28 14:41:20 -0700

    script work

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # run.sh
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
 
 # echo "Starting Server...";
 # ../dump1090/dump1090 --net
+../dump1090/dump1090 --net --interactive &
 
+sleep 1s
 echo "Starting Map...";
-.venv/bin/activate
+bash src/.venv/bin/activate
 python src/app.py
