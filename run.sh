cc37a281ace89033d0cb1cef118855b229544188
John Wyman <jwyman482@hotmail.com>
2022-10-05 16:05:43 -0700

    silented out dump's output in run.sh

--- run.sh
+++ run.sh
@@ -5,9 +5,8 @@ trap "kill 0" EXIT
 
 # echo "Starting Server...";
 # ../dump1090/dump1090 --net
-~/radar/dump1090/dump1090 --net &
+~/radar/dump1090/dump1090 --net --quiet & > /dev/null
 
 sleep 1s
 echo "Starting Map...";
-bash ~/radar/Mobile_ADS-B_Tracker/src/.venv/bin/activate
 python ~/radar/Mobile_ADS-B_Tracker/src/app.py
