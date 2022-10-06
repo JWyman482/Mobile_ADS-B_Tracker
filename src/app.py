f16de6d640263ee3984c27eee3d73f898fa5dd24
John Wyman <jwyman4821@gmail.com>
2022-10-06 05:12:38 -0700

    Convert DMS lat/lon to deg

--- src/app.py
+++ src/app.py
@@ -1,34 +1,29 @@
-# from json import decoder
-# from types import NoneType
 import renderer
 import settings
-import decoder
 from pyembedded.gps_module.gps import GPS
+
+def deg_to_dec(coord):
+    # For example, for a DMS lat of 47.4791819
+    deg = int(coord)            # deg = 47
+    mins = (coord % deg) * 100  # mins = 47.91819
+    minsDec = mins / 60         # minsDec = .798637
+    result = deg + minsDec      # result = 47.798637
+    return result
 
 def main():
     print(f"1) Try GPS module\n2) Manually enter GPS coords\n3) Use current Coords ({settings.LAT, settings.LON})")
     choice = input(">")
     if int(choice) == 1:
-        # settings.LAT, settings.LON = decoder.get_lat_lon(settings.HOST)
-        # tempLat, tempLon = decoder.get_lat_lon()
         gps = GPS(port='/dev/ttyACM0', baud_rate=9600)
         coords = gps.get_lat_long()
         print(coords)
-        # if coords is not None:
-        #     settings.LAT = coords[0]
-        #     settings.LON = coords[1] * -1
-        
         i = 1
         while coords is None and i < 100000:
             coords = gps.get_lat_long()
             i += 1
         print(coords)
-        settings.LAT = coords[0]
-        settings.LON = coords[1] * -1
-
-        # print(f"Lat: {}, Lon: {tempLon}")
-        # if tempLat != 0:
-        #     settings.LAT, settings.LON = tempLat, tempLon
+        settings.LAT = deg_to_dec(coords[0])
+        settings.LON = deg_to_dec(coords[1]) * -1
         
     elif int(choice) == 2:
         latInput = input("Enter your latitude. Example - 46.333221: ")
